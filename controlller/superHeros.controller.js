fda966a273cfd1e06d2a9bb6b15f110e46e8c0ce
SantyCr <77855047+SantyCr@users.noreply.github.com>
2022-05-21 13:31:22 -0500

    Update superHeros.controller.js

--- controlller/superHeros.controller.js
+++ controlller/superHeros.controller.js
@@ -1,10 +1,66 @@
-const express = require('express');
-const { getHeroes } = require('../model/superHeroes.model');
+const conection = require("./intacia.BD/baseDeDatos");
 
-const router = express.Router();
+function superHeroes() {
+    const insert = "CREATE TABLE heroes (id int, name varchar, locaized_name varchar);"
+        conection.query(insert, (err,res) => {
+            if (err) {
+                console.log("Ya existe la base de datos");
+            } else {
+                console.log("Se creo la tabla satifactoriamente");
+            }
+        })  
+}
 
-router.get("/sh",(req, res) => {
-    res.json(getHeroes())
-})
+function getHeroes() {
+    const query = "SELECT * FROM heroes"
+    conection.query(query,(err,res) => {
+        if (err) {
+            console.log("Error al hacer la consulta");
+        } else {
+            console.log(res);
+            return res
+            
+        }
+    })
+}
+function postHeroes(result) {
+    const insert = `INSERT INTO heroes (id,name,localized_name)VALUES ${result.id},${result.name},${result.localized_name}`
+    conection.query(query,(err,res) => {
+        if (err) {
+            console.log("Error al hacer la isercion de datos");
+        } else {
+            console.log(res);
+            return res
+            
+        }
+    })
+}
+function putHeroes() {
+    const query = "SELECT * FROM heroes"
+    conection.query(query,(err,res) => {
+        if (err) {
+            console.log("Error al hacer la consulta");
+        } else {
+            console.log(res);
+            return res
+            
+        }
+    })
+}
 
-module.exports = router;
+function deleteHeroes() {
+    const query = "SELECT * FROM heroes"
+    conection.query(query,(err,res) => {
+        if (err) {
+            console.log("Error al hacer la consulta");
+        } else {
+            console.log(res);
+            return res
+            
+        }
+    })
+}
+
+
+
+module.exports = {superHeroes, getHeroes, postHeroes,putHeroes, deleteHeroes}
