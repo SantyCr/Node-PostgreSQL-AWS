68ca42181f6ba042dbe31e1769d47ba5fd255f64
SantyCr <77855047+SantyCr@users.noreply.github.com>
2022-05-25 14:41:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { application } = require('express');
 const heroesRouter = require('./controlller/superHeros.controller');
+const { superHeroes } = require('./model/superHeroes.model');
 const app = express()
 
 
@@ -18,5 +19,8 @@ app.disable('x-powered-by');
 //Rutas
 app.use(heroesRouter);
 
+superHeroes()
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
