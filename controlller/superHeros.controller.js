4cc786d7b899d440a9f51acd8f1f744f936bd3be
SantyCr <77855047+SantyCr@users.noreply.github.com>
2022-05-24 23:44:40 -0500

    Update superHeros.controller.js

--- controlller/superHeros.controller.js
+++ controlller/superHeros.controller.js
@@ -1,66 +1,20 @@
-const conection = require("./intacia.BD/baseDeDatos");
+const express = require('express');
+const { getHeroes } = require('../model/superHeroes.model');
 
-function superHeroes() {
-    const insert = "CREATE TABLE heroes (id int, name varchar, locaized_name varchar);"
-        conection.query(insert, (err,res) => {
-            if (err) {
-                console.log("Ya existe la base de datos");
-            } else {
-                console.log("Se creo la tabla satifactoriamente");
-            }
-        })  
-}
+const router = express.Router();
 
-function getHeroes() {
-    const query = "SELECT * FROM heroes"
-    conection.query(query,(err,res) => {
-        if (err) {
-            console.log("Error al hacer la consulta");
-        } else {
-            console.log(res);
-            return res
-            
-        }
-    })
-}
-function postHeroes(result) {
-    const insert = `INSERT INTO heroes (id,name,localized_name)VALUES ${result.id},${result.name},${result.localized_name}`
-    conection.query(query,(err,res) => {
-        if (err) {
-            console.log("Error al hacer la isercion de datos");
-        } else {
-            console.log(res);
-            return res
-            
-        }
-    })
-}
-function putHeroes() {
-    const query = "SELECT * FROM heroes"
-    conection.query(query,(err,res) => {
-        if (err) {
-            console.log("Error al hacer la consulta");
-        } else {
-            console.log(res);
-            return res
-            
-        }
-    })
-}
-
-function deleteHeroes() {
-    const query = "SELECT * FROM heroes"
-    conection.query(query,(err,res) => {
-        if (err) {
-            console.log("Error al hacer la consulta");
-        } else {
-            console.log(res);
-            return res
-            
-        }
-    })
-}
+router.get("/sh",(req, res) => {
+    res.json(getHeroes())
+})
+router.post("/sh",(req, res) => {
+    res.json(getHeroes())
+})
+router.put("/sh",(req, res) => {
+    res.json(getHeroes())
+})
+router.delete("/sh",(req, res) => {
+    res.json(getHeroes())
+})
 
 
-
-module.exports = {superHeroes, getHeroes, postHeroes,putHeroes, deleteHeroes}
+module.exports = router;
